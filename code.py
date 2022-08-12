392455f2c85f525ab45114165c804236a6e91619
ALFA <123muntadher972GMAIL.COM>
2022-08-12 20:55:33 +0300

    2'nd commit

--- code.py
+++ code.py
@@ -3,13 +3,19 @@
 def Rmv(txt,Rmv_char):
     return txt.replace(Rmv_char,"")  if Rmv_char in txt else f"{Rmv_char} doesn't belong to {txt}!"
 print(Rmv(txt=input('Write your text:'),Rmv_char=input("Charater to delete: ")))
-#bug, if the sentence has two repeated charater, it will delete them both
-
+#Q: if I want the code to delete A and This text is provided "AAA". Everything will be deleted, your question didn't cover this point actually and
+#I'm sort of confused whether I should set the code to only del the first char or leave it as it is.
 
 
 #Q2/Write a program to find all prime numbers up to a given range of numbers?
 
-
+def prime_num(n):
+    ini = 0
+    for x in range(2, n):
+        ini = ini + 1 if n % x == 0 else ""
+    return ini   
+n = int(input("Wrte down thenumber=  "))
+#print("Prime num.") if prime_num(n) == 0 else print("Not prime num.")
 
 #Q3/write a function that count how many the given character repeated in a given string?
 from itertools import count
